65f09e407d80762dec1d26b93b502f2e5cbc078e
MaiconRomario <maiconromario389@hotmail.com>
2025-08-25 12:32:07 -0300

    Creading firsts routes

--- order_routes.py
+++ order_routes.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
 order_router = APIRouter(prefix="/orders", tags=["orders"])
+
+@order_router.get('/')
+async def get_order():
+    return {"message": "Voçe acessou as ordens de pedidos."}

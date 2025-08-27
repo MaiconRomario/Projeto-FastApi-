53f026b52dd8ee0dc26b5ffd3f54589beb9c4d4a
MaiconRomario <maiconromario389@hotmail.com>
2025-08-27 16:03:42 -0300

    HTTPException and Schemas

--- auth_routes.py
+++ auth_routes.py
@@ -1,8 +1,10 @@
 # type: ignore
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_session
 from main import bcrypt_context
+from schemas import UserSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -12,14 +14,14 @@ async def home():
 
 
 @auth_router.post('/create_user')
-async def create_user(email: str, password: str, name: str,session = Depends(get_session)):
-    user = session.query(User).filter(User.email==email).first()
+async def create_user(user_schema: UserSchema ,session : Session = Depends(get_session)):
+    user = session.query(User).filter(User.email==user_schema.email).first()
     if user:
-        return {'message': "There is already a user with that email"}
+        raise HTTPException(status_code=400, detail="Email of the already registered user")
     else:
-        password_encrypted = bcrypt_context.hash(password)
-        new_user = User(name, email, password_encrypted)
+        password_encrypted = bcrypt_context.hash(user_schema.password)
+        new_user = User(user_schema.name, user_schema.email, password_encrypted, user_schema.active, user_schema.admin)
         session.add(new_user)
         session.commit()
-        return {'message': "User creating success"}
+        return {'message': f"User creating success {user_schema.email}"}
     
